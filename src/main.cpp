f921d9e22f0170b16e9f06f77b553204fb99fb4e
gabriel garcia jaubert <alu0101240374@ull.edu.es>
2021-05-08 13:22:03 +0100

    tables

--- src/main.cpp
+++ src/main.cpp
@@ -12,9 +12,11 @@ int main(int args, char* argv[]) {
     std::ofstream file;
     file.open("tables/grasp-tables.md", std::ofstream::app);
     std::vector<std::string> sizes = {"15", "20", "30"};
-    file << "| ID | n | k | m | z | S | CPU(μs)\n";
+    file << "| ID | n | k | m | Max_iter | LRC | z | S | CPU(μs)\n";
     file << "|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|:-:|\n";
     std::shared_ptr<Strategy> ptr;
+    // ptr = std::make_shared<Greedy>();
+    // ptr->computeSolution(test);
     ptr = std::make_shared<Grasp>();
     Mdp result;
     for (int s = 0; s < sizes.size(); s++) {
@@ -26,12 +28,12 @@ int main(int args, char* argv[]) {
         fileName += ".txt";
         for (int m = 2; m < 5; m++) {
           Mdp newMdp(fileName, m);
-          ptr = std::make_shared<Greedy>();
+          ptr = std::make_shared<Grasp>();
           auto start = high_resolution_clock::now();
           result = ptr->computeSolution(newMdp);
           auto stop = high_resolution_clock::now();
           auto duration = duration_cast<microseconds>(stop - start);
-          file << fileName << "|" << sizes[s] << "|" << i << "|" << m  << "|" << result.calculateZ() << "|" << result.toString() << "|" << duration.count() << "\n"; 
+          file << fileName << "|" << sizes[s] << "|" << i << "|" << m << "|10|" << K << "|" << result.calculateZ() << "|" << result.toString() << "|" << duration.count() << "\n"; 
         }
       }
     }
