add15dae3c81bab9d05da6e3598863abb4bf4d59
gabriel garcia jaubert <alu0101240374@ull.edu.es>
2021-05-16 13:40:16 +0100

    intento UB3

--- lib/branch-bound.hpp
+++ lib/branch-bound.hpp
@@ -26,9 +26,11 @@ class BranchBound {
     Mdp grasp(Mdp object);
     Mdp greedy(Mdp object);
     float calculateUB(Mdp object);
-    float calculateUB2(Mdp object);
-    std::vector<float> zSelVector(Mdp object);
+    float calculateUB2(Mdp object, std::vector<Element> unselected);
+    float calculateUB3(Mdp object, std::vector<Element> unselected);
+    std::vector<float> zSelVector(Mdp object, std::vector<Element> unselected);
     std::vector<float> sort(std::vector<float> selv);
+    std::vector<float> zUnselVector(Mdp object, std::vector<Element> unselected);
 };
 
 #endif
--- src/branch-bound.cpp
+++ src/branch-bound.cpp
@@ -43,17 +43,7 @@ Mdp BranchBound::greedy(Mdp object) {
 }
 
 float BranchBound::calculateUB(Mdp object) {
-  float z1 = object.calculateZ();
-  std::cout << z1 << "\n";
-  float z2 = calculateUB2(object);
-  std::cout << z2 << "\n";
-  return z1;
-}
-
-std::vector<float> BranchBound::zSelVector(Mdp object) {
-  float z = 0;
   std::vector<Element> unselected;
-  std::vector<float> z2Vector;
   for (int i = 0; i < allPoints.size(); i++) {
     for (int j = 0; j < solutions[0].solution.size(); j++) {
       if (allPoints[i].getPosition() == solutions[0].solution[j].getPosition()) {
@@ -63,6 +53,17 @@ std::vector<float> BranchBound::zSelVector(Mdp object) {
       }
     }
   }
+  float z1 = object.calculateZ();
+  std::cout << z1 << "\n";
+  float z2 = calculateUB2(object, unselected);
+  std::cout << z2 << "\n";
+  float z3 = calculateUB3(object, unselected);
+  return z1;
+}
+
+std::vector<float> BranchBound::zSelVector(Mdp object, std::vector<Element> unselected) {
+  float z = 0;
+  std::vector<float> z2Vector;
   for (int i = 0; i < unselected.size(); i++) {
     z = 0;
     for (int j = 0; j < object.solution.size(); j++) {
@@ -82,13 +83,59 @@ std::vector<float> BranchBound::sort(std::vector<float> selv) {
   return selv;
 }
 
-float BranchBound::calculateUB2(Mdp object) {  // siempre que trabajemos con solution.size nos referimos a k
+float BranchBound::calculateUB2(Mdp object, std::vector<Element> unselected) {  // siempre que trabajemos con solution.size nos referimos a k
   int UB2 = 0;
-  std::vector<float> selv;
-  selv = zSelVector(object);
-  selv = sort(selv);
+  std::vector<float> selV;
+  selV = zSelVector(object, unselected);
+  selV = sort(selV);
   for (int i = 0; i < m - object.solution.size(); i++) {
-    UB2 += selv[i];
+    UB2 += selV[i];
   }
   return UB2;
+}
+
+std::vector<float> BranchBound::zUnselVector(Mdp object, std::vector<Element> unselected) {
+  std::vector<float> unselV;
+  float z;
+  for (int i = 0; i < unselected.size(); i++) {
+    z = 0;
+    std::vector<float> dUnsel;
+    for (int j = 0; j < unselected.size(); j++) {
+      if (unselected[i].getPosition() == unselected[j].getPosition()) {
+        continue;
+      }
+      dUnsel.push_back(object.calculateDistance(unselected[i].getPosition(), unselected[j].getPosition()));
+    }
+    float newZ = 0;
+    dUnsel = sort(dUnsel);
+    std::cout << "dUnsel: \n";
+    for (int l = 0; l < dUnsel.size(); l++) {
+      std::cout << dUnsel[l] << " ";
+    }
+    std::cout << "\nLos que se suman de dUnsel: \n";
+    for (int k = 0; k < m - (int)object.solution.size() - 1; k++) {
+      newZ += dUnsel[k];
+      std::cout << dUnsel[k] << " ";
+    }
+    std::cout << "\n";
+    std::cout << "El numero que se divide entre dos: " << newZ << "\n\n";
+    unselV.push_back(newZ * 0.5);
+  }
+  std::cout << "Unsel: \n";
+  for (int i = 0; i < unselV.size(); i++) {
+    std::cout << unselV[i] << " ";
+  }
+  std::cout << "\n";
+  return unselV;
+}
+
+
+float BranchBound::calculateUB3(Mdp object, std::vector<Element> unselected) {
+  float UB3 = 0;
+  std::vector<float> unselV;
+  unselV = zUnselVector(object, unselected);
+  for (int i = 0; i < m - object.solution.size(); i++) {
+    UB3 += unselV[i];
+  }
+  return UB3;
 }
